c0d090b9fb4843e4e0d2fc3a6b018711c4427282
Joe Banford-Jones <0banfj23@solent.ac.uk>
2026-03-25 16:42:14 +0000

    add notification

--- app/src/main/java/com/example/ramanigps/MainActivity.kt
+++ app/src/main/java/com/example/ramanigps/MainActivity.kt
@@ -6,14 +6,20 @@ import android.location.LocationListener
 import android.os.Bundle
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationChannelGroup
+import android.app.NotificationManager
 import android.content.pm.PackageManager
 import android.location.LocationManager
+import android.os.Build
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Column
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
@@ -25,7 +31,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.window.Dialog
 import com.example.ramanigps.ui.theme.RamaniGPSTheme
 import org.maplibre.android.geometry.LatLng
 import org.maplibre.android.maps.Style
@@ -36,6 +41,7 @@ import org.ramani.compose.MapLibre
 class MainActivity : ComponentActivity(), LocationListener {
 
     lateinit var locationManager: LocationManager
+    lateinit var nMgr: NotificationManager
 
     // You must create the style builder as an attribute of the activity and then
     // pass it to composables as a parameter. If you create the style builder on
@@ -44,9 +50,15 @@ class MainActivity : ComponentActivity(), LocationListener {
     
     val viewModel: GPSViewModel by viewModels()
 
+    val channelID = "EMAIL_CHANNEL"
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         checkPermissions()
+        nMgr = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val channel = NotificationChannel(channelID, "Email notifications", NotificationManager.IMPORTANCE_DEFAULT)
+        nMgr.createNotificationChannel(channel)
         setContent {
             RamaniGPSTheme {
                 // state variable for displaying location info
@@ -77,9 +89,6 @@ class MainActivity : ComponentActivity(), LocationListener {
                 }
                 if (dialogVisible) {
                     AlertDialog(
-                        icon = {
-                            Icon(painter=painterResource(org.maplibre.android.R.drawable.maplibre_logo_icon), "Book Tickets")
-                        },
                         title = {
                             Text("Current Position")
                         },
@@ -112,10 +121,13 @@ class MainActivity : ComponentActivity(), LocationListener {
     }
 
     fun checkPermissions() {
+        Log.d("ramanigps1", "checkPermissions()")
+
         val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.POST_NOTIFICATIONS)
         if (permissions.any { checkSelfPermission(it) != PackageManager.PERMISSION_GRANTED }) {
+            Log.d("ramanigps1", "We need to request some permissions")
             val launcher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { isGranted ->
-
+                Log.d("ramanigps1", "Granted status: $isGranted")
                 // With multiple permissions, isGranted is a map and we use the
                 // specific permission as a key (index) to test if that specific
                 // permission has been granted
@@ -130,6 +142,8 @@ class MainActivity : ComponentActivity(), LocationListener {
             // Launch the launcher with the array of requested permissions
             launcher.launch(permissions)
         } else {
+            Log.d("ramanigps1", "All permissions granted already")
+            Toast.makeText(this, "All permissions granted already", Toast.LENGTH_LONG).show()
             startGPS()
         }
     }
@@ -140,8 +154,18 @@ class MainActivity : ComponentActivity(), LocationListener {
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 5f, this)
     }
 
+    var uniqueIdCounter = 0
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onLocationChanged(location: Location) {
         viewModel.latLng = LatLng(location.latitude, location.longitude)
+        uniqueIdCounter++
+        println("got here")
+        val notification = Notification.Builder(this, channelID)
+            .setContentTitle("GPS update")
+            .setContentText("The GPS position has changed")
+            .setSmallIcon(R.drawable.ic_launcher_foreground) // change this icon
+            .build()
+        nMgr.notify(uniqueIdCounter, notification) // uniqueId is a unique ID for this notification
     }
 }
 
